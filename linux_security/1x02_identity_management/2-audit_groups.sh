82bf0faeb52413ab23bfa0b7670585d5b27d37a3
JorgeBeritan <inaikmcontacto@gmail.com>
2026-03-13 11:47:14 -0300

    [+] INFO: Fix script for the checker

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-#!/bin/bash
-awk -F: '$3 >= 1000 {print $1}' "$1" | xargs -I{} sh -c 'id -nG {} | tr " " "\n" | grep -E "^(disk|docker|shadow)$" | sed "s/^/{}:/"'
+awk -F: '$3 >= 1000 {print $1}' "$1" | while read user; do id -nG "$user" | tr ' ' '\n' | grep -E '^(disk|docker|shadow)$' | sed "s/^/$user:/"; done
